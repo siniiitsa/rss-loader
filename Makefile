33db58f388430247f6dc897650f38c517f1fa91e
Sergey <siniiitsa@gmail.com>
2020-06-26 21:10:17 +0300

    main page complete

--- Makefile
+++ Makefile
@@ -1,5 +1,19 @@
+develop:
+	npx webpack-dev-server
+
 install:
 	npm install
 
+build:
+	NODE_ENV=production npx webpack
+
+dev:
+	npx webpack --mode development
+
+test:
+	npm test
+
 lint:
 	npx eslint .
+
+.PHONY: test
---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  context: path.resolve(__dirname, 'src'),
+  watch: true,
+  mode: process.env.NODE_ENV || 'development',
+  entry: {
+    main: './index.js',
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].bundle.js',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html',
+    }),
+    new CleanWebpackPlugin(),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+};
